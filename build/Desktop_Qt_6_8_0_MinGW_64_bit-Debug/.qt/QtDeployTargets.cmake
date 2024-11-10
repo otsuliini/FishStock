bb92ba69a5fefc18ba32d26138f877e81234c24a
boom1401 <135537981+otsuliini@users.noreply.github.com>
2024-11-10 20:39:15 +0200

    Delete build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug directory
    Didn't fuck myself

--- build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_FishStock_FILE C:/Users/otsor/OneDrive/Desktop/FishStock/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/FishStock.exe)
-set(__QT_DEPLOY_TARGET_FishStock_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_FishStock_RUNTIME_DLLS C:/Users/otsor/OneDrive/Desktop/6.8.0/mingw_64/bin/Qt6Widgets.dll;C:/Users/otsor/OneDrive/Desktop/6.8.0/mingw_64/bin/Qt6Gui.dll;C:/Users/otsor/OneDrive/Desktop/6.8.0/mingw_64/bin/Qt6Core.dll)
