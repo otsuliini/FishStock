4df451e142eeca432cc07dfdc702672046287dbc
boom1401 <135537981+otsuliini@users.noreply.github.com>
2024-11-18 18:48:52 +0200

    s

--- src/MoveGen.cpp
+++ src/MoveGen.cpp
@@ -3,8 +3,27 @@
 #include <string>   
 
 int genPawnMoves() {
-
     
 }
 
-int main() {}
+int genKnightMoves() {
+    
+}
+
+int genBishopMoves() {
+    
+}
+
+int genRookMoves() {
+    
+}
+int genQueenMoves() {
+    
+}
+int genKingMoves() {
+    
+}
+
+int genALLMoves() {
+    
+} // I just fucking endded this kids whole carreer blud is cooked, take the fucking LLLL. 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <movegen.cpp>
 
 int main()
 {
